d4031a4a8c7449a3e24b7b784f4c4071f00f1d4a
Melissa Lawrence <mlawrence537@gmail.com>
2021-01-16 20:54:13 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,8 +1,9 @@
-chance_of_rain = 1 
+chance_of_rain = 0.2
 puts "Let's go outside!"
-if chance_of_rain > 0.5
+if chance_of_rain <= 0.25
+  puts "Pack a sun shelter!"
+else if (chance_of_rain > 0.25 && chance_of_rain < 0.75)
   puts "Pack an umbrella!"
 else
-  puts "Enjoy the fine day!"
+  puts "Stay home and read Hegel"
 end
-puts "Oh, and always wear sunscreen!"
